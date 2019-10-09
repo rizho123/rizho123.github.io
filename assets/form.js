527712be1abdaf5c17fb81ad5c4fc51a917bad12
rizho123 <v-riczho@microsoft.com>
2019-10-09 01:01:51 -0700

    updating form

--- assets/form.js
+++ assets/form.js
@@ -1,56 +1,51 @@
-$(document).ready(function() {
-
-    var form = $('#form'),
-        email = $('#email'),
-        subject = $('#subject'),
-        message = $('#message'),
-        info = $('#info'),
-        submit = $("#submit");
+function sendContact() {
     
-    form.on('input', '#email, #subject, #message', function() {
-      $(this).css('border-color', '');
-      info.html('').slideUp();
-    });
-    
-    submit.on('click', function(e) {
-      e.preventDefault();
-      if(validate()) {
-        $.ajax({
-          type: "POST",
-          url: "mailer.php",
-          data: form.serialize(),
-          dataType: "json"
-        }).done(function(data) {
-          if(data.success) {
-            email.val('');
-            subject.val('');
-            message.val('');
-            info.html('Message sent!').css('color', 'green').slideDown();
-          } else {
-            info.html('Could not send mail! Sorry!').css('color', 'red').slideDown();
-          }
+    var valid;	
+    valid = validateContact();
+    if(valid) {
+        jQuery.ajax({
+            url: "contact_mail.php",
+            data:'userName='+$("#userName").val()+'&userEmail='+
+            $("#userEmail").val()+'&subject='+
+            $("#subject").val()+'&content='+
+            $(content).val(),
+            type: "POST",
+            success:function(data){
+                $("#mail-status").html(data);
+            },
+            error:function (){}
         });
-      }
-    });
-    
-    function validate() {
-      var valid = true;
-      var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
-      
-      if(!regex.test(email.val())) {
-        email.css('border-color', 'red');
-        valid = false;
-      }
-      if($.trim(subject.val()) === "") {
-        subject.css('border-color', 'red');
-        valid = false;
-      }
-      if($.trim(message.val()) === "") {
-        message.css('border-color', 'red');
-        valid = false;
-      }
-      
-      return valid;
     }
-  
-  });
+}
+
+function validateContact() {
+    var valid = true;	
+    $(".demoInputBox").css('background-color','');
+    $(".info").html('');
+    if(!$("#userName").val()) {
+        $("#userName-info").html("(required)");
+        $("#userName").css('background-color','#FFFFDF');
+        valid = false;
+    }
+    if(!$("#userEmail").val()) {
+        $("#userEmail-info").html("(required)");
+        $("#userEmail").css('background-color','#FFFFDF');
+        valid = false;
+    }
+    if(!$("#userEmail").val().match(/^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/)) {
+        $("#userEmail-info").html("(invalid)");
+        $("#userEmail").css('background-color','#FFFFDF');
+        valid = false;
+    }
+    if(!$("#subject").val()) {
+        $("#subject-info").html("(required)");
+        $("#subject").css('background-color','#FFFFDF');
+        valid = false;
+    }
+    if(!$("#content").val()) {
+        $("#content-info").html("(required)");
+        $("#content").css('background-color','#FFFFDF');
+        valid = false;
+    }
+    return valid;
+}
--- assets/mail.php
+++ assets/mail.php
@@ -1,16 +1,9 @@
 <?php
-if($_POST) {
-  $to = "richardzhou123@gmail.com"; // your mail here
-  $email = filter_var($_POST["email"], FILTER_SANITIZE_EMAIL);
-  $subject = filter_var($_POST["subject"], FILTER_SANITIZE_STRING);
-  $message = filter_var($_POST["message"], FILTER_SANITIZE_STRING);
-  $body = "Message: $message\nE-mail: $email";
-  
-  if(@mail($to, $subject, $body)) {
-    $output = json_encode(array('success' => true));
-    die($output);
-  } else {
-    $output = json_encode(array('success' => false));
-    die($output);
-  }
-}
+    $toEmail = "richardzhou123@gmail.com";
+    $mailHeaders = "From: " . $_POST["userName"] . "<". $_POST["userEmail"] .">\r\n";
+    if(mail($toEmail, $_POST["subject"], $_POST["content"], $mailHeaders)) {
+        print "<p class='success'>Mail Sent.</p>";
+    } else {
+        print "<p class='Error'>Problem in Sending Mail.</p>";
+    }
+?>
